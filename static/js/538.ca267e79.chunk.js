10e15f13e935b7c13cbbe31a4b6824bf18530b67
NatalisDom <vitveda@gmail.com>
2023-05-04 19:25:55 +0000

    Deploying to gh-pages from @ NatalisDom/goit-react-hw-08-phonebook@1a3c8d896002008839888b30242c335e2d654927 🚀

--- static/js/538.ca267e79.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[538],{538:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(329),c=function(){return(0,r.jsx)("div",{children:"Contacts list"})};function u(){return(0,r.jsx)("div",{children:(0,r.jsx)(c,{})})}}}]);
-//# sourceMappingURL=538.ca267e79.chunk.js.map
